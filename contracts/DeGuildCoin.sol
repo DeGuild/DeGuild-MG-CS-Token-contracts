962223a80d194432b28ec2a63a512a74be8f4bfb
parmcoder <possawat2017@gmail.com>
2021-08-19 00:24:37 +0700

    cannot test deployed contract

--- contracts/DeGuildCoin.sol
+++ contracts/DeGuildCoin.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./DeGuildCoinERC777.sol";
 
-contract DeguildCoin is DeGuildCoinERC777 {
+contract DeGuildCoin is DeGuildCoinERC777 {
     address[] arr;
 
     constructor()
--- test/Token.js
+++ test/Token.js
@@ -1,14 +1,34 @@
 const { expect } = require("chai");
 
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
+  it("Deployment should not assign the total supply of tokens to the owner", async function () {
     const [owner] = await ethers.getSigners();
 
-    const Token = await ethers.getContractFactory("Token");
+    const Token = await ethers.getContractFactory("DeGuildCoin");
 
-    const hardhatToken = await Token.deploy();
+    const DeGuildCoin = await Token.deploy();
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    const ownerBalance = await DeGuildCoin.balanceOf(owner.address);
+    expect(DeGuildCoin.totalSupply()).to.equal(ownerBalance);
+  });
+
+  it("Deployment should be named DeGuild Coin", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("DeGuildCoin");
+
+    const DeGuildCoin = await Token.deploy();
+
+    expect(await DeGuildCoin.name()).to.equal("DeGuild Coin");
+  });
+
+  it("Deployment should be symbolized DGC", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const Token = await ethers.getContractFactory("DeGuildCoin");
+
+    const DeGuildCoin = await Token.deploy();
+
+    expect(await DeGuildCoin.symbol()).to.equal("DGC");
   });
 });

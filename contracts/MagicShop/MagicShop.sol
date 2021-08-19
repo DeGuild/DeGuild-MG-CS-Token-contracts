b53dc0496dfc7f58efc49ca19ea272feec846b19
parmcoder <possawat2017@gmail.com>
2021-08-19 22:33:21 +0700

    working on magic shop, designing interfaces to buy scrolls

---
+++ contracts/MagicShop/MagicShop.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./MagicScrolls.sol";
+
+contract MagicShop {
+
+    string public name;
+    string public owner;
+
+    MagicScrolls public scrolls;
+
+    constructor() {
+        scrolls = new MagicScrolls();
+    }
+}

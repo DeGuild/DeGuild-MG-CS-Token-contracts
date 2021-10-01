91712f75f6a395fdc7b5e8eb51fc534bd4981eb0
parmcoder <possawat2017@gmail.com>
2021-10-01 08:36:27 +0700

    done

--- contracts/SkillCertificates/SkillCertificate.sol
+++ contracts/SkillCertificates/SkillCertificate.sol
@@ -93,23 +93,14 @@ contract SkillCertificate is Context, Ownable, ISkillCertificate {
      *
      * - `tokenId` cannot be non-existence token.
      */
-    function tokenURI(uint256 tokenId)
+    function tokenURI()
         public
         view
         virtual
         override
         returns (string memory)
     {
-        require(
-            _exists(tokenId),
-            "ERC721Metadata: URI query for nonexistent token"
-        );
-
-        string memory baseURI = _baseURI();
-        return
-            bytes(baseURI).length > 0
-                ? string(abi.encodePacked(baseURI, tokenId.toString()))
-                : "";
+        return _baseURI();
     }
 
     /**

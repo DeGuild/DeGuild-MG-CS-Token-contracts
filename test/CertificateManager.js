92208f3773943d499a509c1f35632ba0eb9a3208
parmcoder <possawat2017@gmail.com>
2021-10-05 21:34:45 +0700

    changed how the tokenURI display to ASCII characters

---
+++ test/CertificateManager.js
@@ -0,0 +1,26 @@
+const { expect } = require("chai");
+
+describe("Certificate Manager Contract", function () {
+  
+  before(async function () {
+    this.Certificate = await ethers.getContractFactory("CertificateManager")
+    this.signers = await ethers.getSigners()
+  })
+
+  beforeEach(async function () {
+    this.CertificateManager = await this.Certificate.deploy()
+    await this.CertificateManager.deployed()
+  })
+
+
+  it("should have correct name and symbol and decimal", async function () {
+    const name = await this.CertificateManager.name()
+    const symbol = await this.CertificateManager.symbol()
+    // const uri = await this.CertificateManager.
+    const address = await this.CertificateManager.tokenURI()
+    console.log(address)
+    expect(name, "Introduction to Computer Programming")
+    expect(symbol, "ICCS101")
+  })
+
+});

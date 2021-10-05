0a1647bfbfde25971271f083e86d40d50d99c96f
parmcoder <possawat2017@gmail.com>
2021-10-05 23:17:46 +0700

    trying to find the error

--- contracts/SkillCertificates/SkillCertificate.sol
+++ contracts/SkillCertificates/SkillCertificate.sol
@@ -222,19 +222,22 @@ contract SkillCertificate is Context, Ownable, ISkillCertificate {
     }
 
     function _toAsciiString(address x) internal pure returns (string memory) {
-        bytes memory s = new bytes(40);
-        for (uint256 i = 0; i < 20; i++) {
-            bytes1 b = bytes1(uint8(uint256(uint160(x)) / (2**(8 * (19 - i)))));
-            bytes1 hi = bytes1(uint8(b) / 16);
-            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
-            s[2 * i] = _char(hi);
-            s[2 * i + 1] = _char(lo);
+        uint256 _i = uint256(uint160(x));
+        if (_i == 0) {
+            return "0";
         }
-        return string(abi.encodePacked("0x", string(s)));
-    }
-
-    function _char(bytes1 b) internal pure returns (bytes1 c) {
-        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
-        else return bytes1(uint8(b) + 0x57);
+        uint256 j = _i;
+        uint256 len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint256 k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = bytes1(uint8(48 + (_i % 10)));
+            _i /= 10;
+        }
+        return string(bstr);
     }
 }

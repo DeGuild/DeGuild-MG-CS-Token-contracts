ea931f427c95dc14e67486a12807a5afea8da318
parmcoder <possawat2017@gmail.com>
2021-10-05 23:04:50 +0700

    finalized all tokenURIs and added more lines on deployment script

--- contracts/SkillCertificates/CertificateManager.sol
+++ contracts/SkillCertificates/CertificateManager.sol
@@ -3,14 +3,38 @@ pragma solidity ^0.8.0;
 
 import "./SkillCertificate.sol";
 
-contract CertificateManager is SkillCertificate {
+contract CertificateManager0 is SkillCertificate {
+    constructor()
+        SkillCertificate(
+            "Introduction to Computer Programming",
+            "ICCS101",
+            "https://us-central1-deguild-2021.cloudfunctions.net/readCertificate/",
+            address(0xfE7992D9292491b3Cd7d2F3f25716c3f5f579660),
+            0
+        )
+    {}
+}
+
+contract CertificateManager1 is SkillCertificate {
     constructor()
         SkillCertificate(
             "Database Foundations",
             "ICCS225",
             "https://us-central1-deguild-2021.cloudfunctions.net/readCertificate/",
-            address(0x706787872D518F91fB9B7a86055AB616a2C64958),
+            address(0xfE7992D9292491b3Cd7d2F3f25716c3f5f579660),
             1
+        )
+    {}
+}
+
+contract CertificateManager2 is SkillCertificate {
+    constructor()
+        SkillCertificate(
+            "Algorithms and Tractability",
+            "ICCS312",
+            "https://us-central1-deguild-2021.cloudfunctions.net/readCertificate/",
+            address(0xfE7992D9292491b3Cd7d2F3f25716c3f5f579660),
+            2
         )
     {}
 }

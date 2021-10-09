eb5453e6b462b1933a17dcf0afc569a3265b6dcd
parmcoder <possawat2017@gmail.com>
2021-10-09 22:20:50 +0700

    Changed URI in Magic Shop and certificates

--- scripts/deployCertificateManager.js
+++ scripts/deployCertificateManager.js
@@ -6,20 +6,10 @@ async function main() {
   
     console.log("Account balance:", (await deployer.getBalance()).toString());
   
-    const Token1 = await ethers.getContractFactory("CertificateManager0");
+    const Token1 = await ethers.getContractFactory("CertificateManager");
     const token1 = await Token1.deploy();
   
-    console.log("ICCS101 Token address:", token1.address);
-
-    const Token2 = await ethers.getContractFactory("CertificateManager1");
-    const token2 = await Token2.deploy();
-  
-    console.log("ICCS225 Token address:", token2.address);
-
-    const Token3 = await ethers.getContractFactory("CertificateManager2");
-    const token3 = await Token3.deploy();
-  
-    console.log("ICCS312 Token address:", token3.address);
+    console.log("New Token address:", token1.address);
   }
   
   main()

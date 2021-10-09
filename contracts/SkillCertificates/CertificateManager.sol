758e5417b6f0eaf8ff1b7d85c4d5c31bc6da9b32
parmcoder <possawat2017@gmail.com>
2021-10-10 00:30:58 +0700

    fully functional magic shop

--- contracts/SkillCertificates/CertificateManager.sol
+++ contracts/SkillCertificates/CertificateManager.sol
@@ -6,11 +6,11 @@ import "./SkillCertificate.sol";
 contract CertificateManager is SkillCertificate {
     constructor()
         SkillCertificate(
-            "Introduction to Computer Programming",
-            "ICCS101",
+            "Computer Architecture",
+            "ICCS324",
             "https://us-central1-deguild-2021.cloudfunctions.net/app/readCertificate/",
-            address(0xfE7992D9292491b3Cd7d2F3f25716c3f5f579660),
-            0
+            address(0x09eE5D4916b0c937540F2A5a7fB2621564628Fbf),
+            16
         )
     {}
 }

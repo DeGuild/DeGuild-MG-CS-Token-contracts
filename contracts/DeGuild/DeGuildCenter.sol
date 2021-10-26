8f19189b296997bf49e95f0f935738a1220a7138
parmcoder <possawat2017@gmail.com>
2021-10-26 21:57:03 +0700

    prepared code deployment

--- contracts/DeGuild/DeGuildCenter.sol
+++ contracts/DeGuild/DeGuildCenter.sol
@@ -6,4 +6,13 @@ import "contracts/DeGuild/DeGuild.sol";
 // starting in October.
 contract DeGuildCenter is DeGuild {
 
+    constructor()
+        DeGuild(
+            "Polygon Guild",
+            "PGDG",
+            "https://us-central1-deguild-2021.cloudfunctions.net/guild/readJob/",
+            address(0x4312D992940D0b110525f553160c9984b77D1EF4)
+        )
+    {}
+
 }

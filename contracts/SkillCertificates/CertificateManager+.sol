dbb40d1c408ead0cc8cde3235a4f94189acb7be9
parmcoder <possawat2017@gmail.com>
2021-10-30 17:50:59 +0700

    changed URI

--- contracts/SkillCertificates/CertificateManager+.sol
+++ contracts/SkillCertificates/CertificateManager+.sol
@@ -10,7 +10,7 @@ contract CertificateManager is SkillCertificate, ERC165Storage {
         SkillCertificate(
             "Introduction to Computer Programming",
             "ICCS101",
-            "https://us-central1-deguild-2021.cloudfunctions.net/app/readCertificate/",
+            "https://us-central1-deguild-2021.cloudfunctions.net/public/readCertificate/",
             address(0x1B362371f11cAA26B1A993f7Ffd711c0B9966f70),
             1
         )

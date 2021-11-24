7790bde14a1e342779da2ad69ed2aad36f817271
parmcoder <possawat2017@gmail.com>
2021-11-24 23:59:54 +0700

    deployed every contract

--- contracts/SkillCertificates/V2/CertificateManager+.sol
+++ contracts/SkillCertificates/V2/CertificateManager+.sol
@@ -11,7 +11,7 @@ contract CertificateManagerPlus is SkillCertificatePlus, ERC165Storage {
             "Genius Manager",
             "GM",
             "https://us-central1-deguild-2021.cloudfunctions.net/app/readCertificate/",
-            address(0xC1BbF9Bd648BC0c85aF179FB0C20722f678ac31e)
+            address(0x11786516237f1c7bb3b17C9D70739655cF550cD6)
         )
     {
         _registerInterface(type(ISkillCertificatePlus).interfaceId);
